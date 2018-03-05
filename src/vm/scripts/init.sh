ae6f2cd089a1f2a4f12f4ac0d069f4730c94f47c
Logan Rakai <lmrakai@gmail.com>
2018-03-05 13:11:26 -0700

    Improve comment

--- src/vm/scripts/init.sh
+++ src/vm/scripts/init.sh
@@ -3,5 +3,5 @@
 # install Apache web server
 sudo apt install -y apache2
 
-# Create web asset
+# Create a web asset in the default serving directory
 sudo sh -c 'echo "Served with the help of Terraform" > /var/www/html/index.html'
